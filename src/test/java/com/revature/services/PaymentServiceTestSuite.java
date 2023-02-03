b02ff3093bbcc7a9f4db8a26d9c6c60ab355ee72
khavvia <khavvia@gmail.com>
2023-02-03 15:22:51 -0600

    finished unit testing PaymentServiceTestSuite

--- src/test/java/com/revature/services/PaymentServiceTestSuite.java
+++ src/test/java/com/revature/services/PaymentServiceTestSuite.java
@@ -2,8 +2,10 @@ package com.revature.services;
 
 
 import com.revature.dtos.CreatePaymentRequest;
+import com.revature.dtos.EditPaymentRequest;
 import com.revature.dtos.PaymentResponse;
 import com.revature.exceptions.InvalidUserInputException;
+import com.revature.exceptions.ResourceNotFoundException;
 import com.revature.models.Payment;
 import com.revature.models.User;
 import com.revature.repositories.PaymentRepository;
@@ -12,10 +14,15 @@ import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.security.core.parameters.P;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 
 import java.sql.Date;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
 
+import static org.junit.Assert.fail;
 import static org.mockito.Mockito.*;
 
 @ExtendWith(MockitoExtension.class)
@@ -23,6 +30,10 @@ public class PaymentServiceTestSuite {
     private static PaymentService paymentService;
     private static UserService userService;
     private static PaymentRepository paymentRepository;
+
+    public static User dummyUser = new User(1, "email", "pass", "first", "last", true, true, "token");
+
+    public static User dummyUser2 = new User(2, "email2", "pass2", "first2", "last2", true, true, "token2");
 
     @BeforeAll
     static void setup() {
@@ -66,24 +77,49 @@ public class PaymentServiceTestSuite {
 
     @Test
     void testFindPaymentById(){
-        //paymentRepository.findById(1).orElseThrow(() -> new RuntimeException("No payment with this ID found."));
-        
+        Payment test = new Payment();
+        when(paymentRepository.findById("1")).thenReturn(Optional.of(test));
+        paymentService.findPaymentById("1");
+        verify(paymentRepository, times(1)).findById("1");
 
     }
 
     @Test
     void testUpdatePayment(){
+        when(paymentRepository.findById(anyString())).thenReturn(Optional.of(new Payment("2", "a", "b", new Date(1), dummyUser)));
+        try{
+            paymentService.updatePayment(new EditPaymentRequest(), dummyUser2);
+            fail("Exception should have been throw");
+        } catch(ResourceNotFoundException e){
+            Assertions.assertNotNull(e);
+        }
+    }
 
+    @Test
+    void updatePayment_ExceptionThrown(){
+        when(paymentRepository.findById(anyString())).thenReturn(Optional.of(new Payment("2", "a", "b", new Date(1), dummyUser)));
+        try{
+            paymentService.updatePayment(new EditPaymentRequest(), dummyUser2);
+            fail("Exception should have been throw");
+        } catch(ResourceNotFoundException e){
+            Assertions.assertNotNull(e);
+        }
     }
 
     @Test
     void testDeletePayment(){
-
+        when(paymentRepository.findById(anyString())).thenReturn(Optional.of(new Payment("9", "a", "b", new Date(1), dummyUser)));
+        paymentService.deletePayment("cheese", dummyUser);
+        verify(paymentRepository, times(1)).delete(any(Payment.class));
     }
 
     @Test
     void testFindAllByUser(){
-
+        List<Payment> pList = new ArrayList<>();
+        when(paymentRepository.findCardsByUser(anyInt())).thenReturn(Optional.of(pList));
+        List<PaymentResponse> rList = paymentService.findAllByUser(4);
+        verify(paymentRepository, times(1)).findCardsByUser(anyInt());
+        Assertions.assertNotNull(rList);
     }
 
 }
